c3d3490c38157b78cd6d60f8d242accabdaa3bf0
alistairgray <alistair.g.gray@gmail.com>
2021-03-26 12:56:21 +1100

    initial commit

---
+++ src/components/SandboxApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SandboxApp extends React.Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        ) // return()
+    } // render()
+} // class SandboxApp
